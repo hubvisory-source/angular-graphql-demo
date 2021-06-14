de8d50a576d522f92b2771d1c3a1629c53dfde91
Myriam Dubois <myriam.dubois@hubvisory.com>
2021-06-14 11:47:11 +0200

    feat: create user componant with static info

---
+++ src/app/user/user.component.ts
@@ -0,0 +1,17 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-user',
+  templateUrl: './user.component.html',
+  styleUrls: ['./user.component.css'],
+})
+export class UserComponent implements OnInit {
+  user = { firstName: '', lastName: '' };
+
+  constructor() {}
+
+  ngOnInit(): void {
+    this.user.firstName = 'Jean';
+    this.user.lastName = 'Bonbeurre';
+  }
+}
