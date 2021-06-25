c9c026c738c483624bc66376a4f4184fbe2d7a6d
Myriam Dubois <myriam.dubois@hubvisory.com>
2021-06-25 17:47:30 +0200

    style: add style

--- src/app/user/user.component.ts
+++ src/app/user/user.component.ts
@@ -11,7 +11,7 @@ export class UserComponent implements OnInit {
   constructor() {}
 
   ngOnInit(): void {
-    this.user.firstName = 'Jean';
-    this.user.lastName = 'Bonbeurre';
+    this.user.firstName = 'John';
+    this.user.lastName = 'Doe';
   }
 }
