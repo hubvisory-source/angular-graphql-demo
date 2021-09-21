94c75001cdeeb5287e3be908d573a6958b8accd0
Myriam Dubois <myriam.dubois@hubvisory.com>
2021-09-21 18:53:12 +0200

    Refacto: correct tests on user component to use only testing library

--- src/app/user/user.component.spec.ts
+++ src/app/user/user.component.spec.ts
@@ -1,14 +1,10 @@
-import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { NetworkStatus } from '@apollo/client/core';
-import { of } from 'rxjs';
 import { render, screen } from '@testing-library/angular';
-
+import { of } from 'rxjs';
 import { UserComponent } from './user.component';
 import { UserService } from './user.service';
 
 describe('UserComponent', () => {
-  let component: UserComponent;
-  let fixture: ComponentFixture<UserComponent>;
   let mockedGetUserFromId = jest.fn().mockReturnValue(
     of({
       data: {
@@ -26,32 +22,15 @@ describe('UserComponent', () => {
     getUserFromId: mockedGetUserFromId,
   };
 
-  beforeEach(() => {
-    jest.clearAllMocks();
-    TestBed.configureTestingModule({
-      declarations: [UserComponent],
-    });
-    TestBed.overrideComponent(UserComponent, {
-      set: {
-        providers: [
-          {
-            provide: UserService,
-            useValue: mockedUserService,
-          },
-        ],
-      },
-    });
-  });
-
-  it('should create', () => {
-    fixture = TestBed.createComponent(UserComponent);
-    component = fixture.componentInstance;
-    fixture.detectChanges();
-    expect(component).toBeTruthy();
-  });
-
   it('should display the firstname and lastname of the user', async () => {
-    await render(UserComponent);
+    await render(UserComponent, {
+      componentProviders: [
+        {
+          provide: UserService,
+          useValue: mockedUserService,
+        },
+      ],
+    });
     expect(screen.getByText(/John/i)).toBeTruthy();
     expect(screen.getByText(/Doe/i)).toBeTruthy();
   });
